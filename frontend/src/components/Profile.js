fb85b1811f0728fa7e46eb92599ff57bc6dfa860
siruliina <91068804+siruliina@users.noreply.github.com>
2023-03-01 13:24:03 +0200

    Logged user sent to profile via props

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import { Stack, Row, Col, Button, Table, Nav } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Stack, Row, Col, Button } from 'react-bootstrap';
 import itemService from '../services/item';
-import { LinkContainer } from 'react-router-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = (loggedUser) => {
   const [userItems, setUserItems] = useState([]);
   const [user, setUser] = useState({});
   const navigate = useNavigate();
@@ -16,7 +15,7 @@ const Profile = () => {
   const getUser = async () => {
     const items = await itemService.getUserItems()
     setUserItems(items)
-    setUser(JSON.parse(window.localStorage.getItem('loggedMarketplaceUser')));
+    setUser(loggedUser.loggedUser);
   };
 
   const deleteItem = async (event, itemToBeRemoved) => {
